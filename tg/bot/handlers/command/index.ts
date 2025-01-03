5cff108ed0980cea924d1e4660f54f4ef1114eca
Daniel <pangpangcoder@gmail.com>
2025-01-03 22:01:54 +0800

    Delete tg directory

--- tg/bot/handlers/command/index.ts
+++
@@ -1,5 +0,0 @@
-export * from "./startCommand";
-export * from "./smartAddressCommand";
-export * from "./addSmartAddressCommand";
-export * from "./removeSmartAddressCommand";
-export * from "./searchTokenInTwitter";
